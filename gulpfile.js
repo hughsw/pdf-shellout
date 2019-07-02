773a4398460b61cd52fca36c661a9d0261295f8c
Hugh Secker-Walker <hsw@hodain.net>
2019-07-02 17:14:37 -0400

    chore(pdfjs): Cleanup comments and packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,10 @@ const http = require('http');
 const gulp = require('gulp');
 const st = require('st');
 
-/*
-  gulp.task('server', function () {
-  console.log();
-  console.log('### Starting local server');
-
-  var WebServer = require('./test/webserver.js').WebServer;
-  var server = new WebServer();
-  server.port = 8888;
-  server.start();
-  });
-*/
-
 gulp.task('server', done => {
   const port = '8088';
-  const statics =     st({path: './', cache:false});
-  const server = http.createServer(
-    statics
-  );
+  const statics = st({ path: './', cache: false });
+  const server = http.createServer(statics);
   server.on('listening', () => console.log('server: listening:', server.address()));
   server.listen(port, done);
 });
--- index2.js
+++ index2.js
@@ -2,23 +2,86 @@
 
 const pdfjsLib = require('pdfjs-dist');
 
-const go = async () => {
-  const filename = 'DesignerSpec-SG-009-01.pdf';
+const shellfile = async filename => {
+  console.log(filename);
+  return doPdf({
+    tag: filename,
+    //  filename,
+    pdf: await pdfjsLib.getDocument({
+      url: filename,
+      disableFontFace: true,
+      nativeImageDecoderSupport: 'none',
+    }).promise,
+  });
+};
+
+const shellfileX = async filename => {
+  // const filename = 'DesignerSpec-SG-009-01.pdf';
   console.log('filename:', filename);
   const pdf = await pdfjsLib.getDocument(filename).promise;
-  const page = await pdf.getPage(1);
-  const operatorList = await page.getOperatorList();
+  return  doPdf({
+    filename,
+    pdf,
+  });
+/*
+
+  return;
 
   const index = 11;
   console.log('op:', operatorList.fnArray[index]);
   const [args0] = operatorList.argsArray[index];
   args0.forEach(item => console.log(JSON.stringify(item)));
   return 0;
+*/
 };
 
-go()
+const range = (start, end, length = end - start) => Array.from({ length }, (_, index) => start + index);
+
+const doPdf = async ({ tag, pdf }) => {
+  console.log(tag, 'numPages:', pdf.numPages);
+
+  return Promise.all(range(1, pdf.numPages + 1).map(async pageNum => {
+    const page = await pdf.getPage(pageNum);
+    return doPage({
+      tag: `${tag}::${pageNum}`,
+      pdf,
+      page,
+    });
+  }));
+
+/*
+  let pageNum = 0;
+  while( ++pageNum <= pdf.numPages) {
+    const page = await pdf.getPage(pageNum);
+    await doPage({
+      filename,
+      pdf,
+      page,
+    });
+  }
+*/
+};
+
+const doPage = async ({ tag, pdf, page }) => {
+  console.log(tag, 'page:', Object.keys(page));
+
+  const operatorList = await page.getOperatorList();
+  console.log(tag, 'operatorList.fnArray.length:', operatorList.fnArray.length, 'operatorList.argsArray.length:', operatorList.argsArray.length);
+  const textContext = await page.getTextContent({
+    normalizeWhitespace: true,
+    disableCombineTextItems: false,
+  });
+  console.log(tag, 'textContext.items.length:', textContext.items.length);
+};
+
+const main = async args => {
+  await Promise.all(args.map(shellfile));
+  return 0;
+};
+
+main(process.argv.slice(2))
   .catch(err => {
     console.log('err:', err);
     return 1;
   })
-  .then(code => process.exit(code))
+  .then(code => process.exit(code));
